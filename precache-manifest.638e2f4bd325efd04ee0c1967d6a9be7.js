52b0ff19203788aff9796bfdf4e06760ca314c3d
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-02-25 16:47:09 +0100

    Updates

---
+++ precache-manifest.638e2f4bd325efd04ee0c1967d6a9be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fa7eba155e12e19fa64e2e913c19c6",
+    "url": "/movie-last/index.html"
+  },
+  {
+    "revision": "9170fecfdb1b79fb6886",
+    "url": "/movie-last/static/css/main.a1e2ad29.chunk.css"
+  },
+  {
+    "revision": "6cd661c1566dd11cc0fd",
+    "url": "/movie-last/static/js/2.aacbe421.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-last/static/js/2.aacbe421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9170fecfdb1b79fb6886",
+    "url": "/movie-last/static/js/main.8f44b47c.chunk.js"
+  },
+  {
+    "revision": "362c5a288af7e0af9396",
+    "url": "/movie-last/static/js/runtime-main.f63d33ac.js"
+  }
+]);
